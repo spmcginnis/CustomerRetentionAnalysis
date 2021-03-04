bafc9366467961a1bb72b4e5306c11156ba58b44
spm <spmcginnis@users.noreply.github.com>
2021-03-04 13:35:25 -0600

    make_table_if_new implemented on generatecustomervisits.py

--- GenerateCustomerVisits.py
+++ GenerateCustomerVisits.py
@@ -8,14 +8,14 @@ import pyodbc
 #   and generates 'visits' which are dates associated with customer IDs. 
 # make_table_if_new() with arg (as dictionary) for colums
 # Add those values to the table
-# Do I need to give the customers table a primary key or is it automatically generated?
 
 visits = 0
 
 
 def get_random_customer_id(connection, table_name):
     # TODO get random customer_id from existing DB
-    return RI(0,10000) # TEMP placeholder
+    customer_id = 0
+    return customer_id
 
 # Returns a list of strings
 def generate_visits_over_date_range(first, last, min, max):
@@ -26,19 +26,45 @@ def generate_visits_over_date_range(first, last, min, max):
         visits = RI(min, max)
         
         for visit in range(0, visits):
-            customer_id = get_random_customer_id('temp', 'temp')
+            customer_id = RI(0,10000) # TEMP placeholder for get_random_customer_id(connection, table_name)
             customer_visits.append(f"{current_date}, {customer_id}")
         
         first += timedelta(days=1)
         
     return customer_visits
 
+# TODO feed the column names in as args
+def make_table_if_new(table_name, connection):
+    cursor = connection.cursor()
+    if cursor.tables(table=table_name, tableType='TABLE').fetchone():
+        print(f"Warning: {table_name} already exists. No changes were commited.")
+    else:
+        print(f"Creating table name {table_name}")
+
+        # Creating a primary key:
+        # https://docs.microsoft.com/en-us/sql/relational-databases/tables/create-primary-keys?view=sql-server-ver15
+        cursor.execute(f'''
+                        CREATE TABLE {table_name}
+                        (
+                            visit_id int IDENTITY (1,1) NOT NULL,
+                            customer_id int NOT NULL,
+                            CONSTRAINT PK_{table_name}_visit_id PRIMARY KEY CLUSTERED (visit_id),
+                            CONSTRAINT FK_{table_name}_customer_id FOREIGN KEY (customer_id)
+                                REFERENCES customers (customer_id)
+                                ON DELETE CASCADE
+                                ON UPDATE CASCADE,
+                            date datetime
+                        )
+                       ''')
+        connection.commit()
 
 if __name__ == "__main__":
     connection = pyodbc.connect('Driver={SQL Server};'
                             'Server=GANDHARA\SQLEXPRESS;'
                             'Database=TestDB;'
                             'Trusted_Connection=yes;')
+    
+    make_table_if_new("customer_visits", connection)
     
     print(generate_visits_over_date_range(date(2018,1,1), date(2019,12,31), 3, 5))
     
