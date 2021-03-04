badd77345e759644fe5c77b410d0080497ad0ec0
spm <spmcginnis@users.noreply.github.com>
2021-03-04 16:05:03 -0600

    generate visits now draws actual customer_id from the DB

--- GenerateCustomerVisits.py
+++ GenerateCustomerVisits.py
@@ -9,24 +9,30 @@ import pyodbc
 # make_table_if_new() with arg (as dictionary) for colums
 # Add those values to the table
 
-visits = 0
-
-
-def get_random_customer_id(connection, table_name):
-    # TODO get random customer_id from existing DB
-    customer_id = 0
-    return customer_id
+def get_customer_ids(connection):
+    customer_id_list = []
+    cursor = connection.cursor()
+    command = '''
+        SELECT customer_id
+        FROM customers
+        '''
+    rows = cursor.execute(command).fetchall()
+    for row in rows:
+        customer_id_list.append(row.customer_id)
+    return customer_id_list
 
 # Returns a list of strings
-def generate_visits_over_date_range(first, last, min, max):
+def generate_visits_over_date_range(id_list, first, last, min, max):
     customer_visits = []
+    
+    customer_count = len(id_list)
     
     while first <= last:
         current_date = first.strftime("%Y-%m-%d")
         visits = RI(min, max)
         
         for visit in range(0, visits):
-            customer_id = RI(0,10000) # TEMP placeholder for get_random_customer_id(connection, table_name)
+            customer_id = id_list[RI(0,customer_count)]
             customer_visits.append(f"{current_date}, {customer_id}")
         
         first += timedelta(days=1)
@@ -66,7 +72,8 @@ if __name__ == "__main__":
     
     make_table_if_new("customer_visits", connection)
     
-    print(generate_visits_over_date_range(date(2018,1,1), date(2019,12,31), 3, 5))
+    id_list = get_customer_ids(connection)
+    print(generate_visits_over_date_range(id_list, date(2018,1,1), date(2018,1,3), 3, 5))
     
     # TEMP # TESTING
     # cursor = connection.cursor()
